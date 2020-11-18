3809b921468b6074abe798241b3a4027b0acdec1
chnirt <chnirt@UrbanOSs-MacBook-Pro.local>
2020-11-18 13:49:02 +0700

    Update shortcuts

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-window.self.addEventListener('fetch', function (event) {
+this.self.addEventListener('fetch', function (event) {
     event.respondWith(
         caches.match(event.request).then(function (response) {
             // Cache hit - return response
